772868bd38233a4614dafd389d07ffc13f477493
whynot <aminfury@mail.ru>
2023-09-28 03:56:23 +0300

    changed backend url

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "http://localhost:3001"
+    //baseURL: "http://localhost:3001",
+    baseURL: "http://restapi5.onrender.com"
+
 })
 
 export default instance
